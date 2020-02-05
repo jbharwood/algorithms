208db84e69749d08b54568de9d92bf62df5c95bc
github username <josephblakeharwood@gmail.com>
2020-02-04 20:38:57 -0500

    updated isPalindrome

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -1,9 +1,12 @@
 // var isPalindrome = function(x) {
 // return x.toString().split("").reverse().join("") == x
 // };
-const isPalindrome = (x) => {
-  return x.toString().split("").reverse().join("") == x
+const isPalindrome = (s) => {
+  let str = s.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase()
+  return str.split("").reverse().join("") === str
 };
 
-console.log(isPalindrome("aba"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
 console.log(isPalindrome("abab"));
+console.log(isPalindrome("aba"));
