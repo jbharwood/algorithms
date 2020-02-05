a9b66d1e492adca8615566c213067aec9d1293a3
github username <josephblakeharwood@gmail.com>
2020-02-04 21:27:10 -0500

    added explanation

--- HouseRobber.js
+++ HouseRobber.js
@@ -14,3 +14,13 @@ var rob = function(nums) {
 console.log(rob([1,2,3,1]));
 console.log(rob([2,7,9,3,1]));
 console.log(rob([2,1,1,2]));
+
+// You are a professional robber planning to rob houses along a street. Each house 
+// has a certain amount of money stashed, the only constraint stopping you from
+// robbing each of them is that adjacent houses have security system connected and
+// it will automatically contact the police if two adjacent houses were broken
+// into on the same night.
+//
+// Given a list of non-negative integers representing the amount of money of each
+// house, determine the maximum amount of money you can rob tonight without
+// alerting the police.
