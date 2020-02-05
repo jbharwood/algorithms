08c98d4a11669cda99663f28318f97e0e4627b6a
github username <josephblakeharwood@gmail.com>
2020-02-04 20:13:34 -0500

    added FactorialTrailingZeroes

---
+++ FactorialTrailingZeroes.js
@@ -0,0 +1,13 @@
+var trailingZeroes = function(n) {
+  var totalZeros = 0;
+
+  while(n > 0) {
+    n = Math.floor(n/5); //Ex: if n = (125); (125) / 5 = {25} ; (25)/5 = {5} ; (5)/5 = {1} ; totalZeros = 25 + 5 + 1 = 31;
+    totalZeros += n;
+  }
+  return totalZeros;
+};
+
+console.log(trailingZeroes(3));
+console.log(trailingZeroes(5));
+console.log(trailingZeroes(7));
