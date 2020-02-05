308a116bbb913118d95ba3162f4306856d11bc96
github username <josephblakeharwood@gmail.com>
2020-02-04 21:31:16 -0500

    added explanation

--- IntersectionOfTwoArraysII.js
+++ IntersectionOfTwoArraysII.js
@@ -18,3 +18,9 @@ console.log(intersect([1,2,2,1], [2,2]));
 console.log(intersect([4,9,5], [9,4,9,8,4]));
 console.log(intersect([1], [1]));
 console.log(intersect([1,2], [1,1]));
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+//
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
