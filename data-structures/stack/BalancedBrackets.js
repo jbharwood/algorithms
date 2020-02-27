81e739360997882bea13edc61676291abc5b2a67
github username <josephblakeharwood@gmail.com>
2020-02-26 20:37:42 -0500

    completed balancedbrackets

---
+++ data-structures/stack/BalancedBrackets.js
@@ -0,0 +1,31 @@
+function isBalanced(s) {
+  var result = 'YES';
+  var stack = [];
+  s.split('').forEach(function(val) {
+    switch(val) {
+      case '{':
+        stack.push('}');
+        break;
+      case '[':
+        stack.push(']');
+        break;
+      case '(':
+        stack.push(')');
+        break;
+      default:
+        var test = stack.pop();
+
+        if (val !== test) {
+          result = 'NO';
+        }
+    }
+  })
+  if (stack.length) {
+    result = 'NO';
+  }
+  return result;
+}
+
+console.log(isBalanced("{[()]}"));
+console.log(isBalanced("{[(])}"));
+console.log(isBalanced("{{[[(())]]}}"));
